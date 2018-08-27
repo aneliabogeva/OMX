ac0176a66498fe6bd40ed64928b4ee79c6b8343b
Atanas Rusinov <atrusinov@gmail.com>
2018-08-27 21:32:31 +0300

    Added moderator area

--- OMX.Web/Areas/Admin/Controllers/PropertiesController.cs
+++ OMX.Web/Areas/Admin/Controllers/PropertiesController.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using OMX.Common.Property.BindingModels;
 using OMX.Common.Property.ViewModels;
 using OMX.Services.Contracts;
 
 namespace OMX.Web.Areas.Admin.Controllers
 {
-    public class PropertiesController : AdminController
+    [Authorize(Roles = "Administrator,Moderator")]    
+    [Area("Admin")]
+    public class PropertiesController : Controller
     {
         private const string LISTING_DELETED_MESSAGE = "The listing has been successfully deleted!";
         private const string LISTING_FEATURED_MESSAGE = "The listing is now featured!";
@@ -25,7 +24,7 @@ namespace OMX.Web.Areas.Admin.Controllers
             this.propertyService = propertyService;
             this.mapper = mapper;
         }
-
+       
         public IActionResult All(string message = null)
         {
             var properties = this.propertyService.GetAllProperties();
